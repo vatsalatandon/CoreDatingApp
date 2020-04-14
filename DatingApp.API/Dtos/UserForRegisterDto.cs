c0978fffd5fb4d03d078effac71e473ff978a9eb
vatsala.tandon <vatsala.tandon@CYG391>
2020-04-14 15:25:34 +0530

    End of Section 3

---
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -0,0 +1,14 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Dtos
+{
+    public class UserForRegisterDto
+    {
+        [Required]
+        public string Username { get; set; }
+        [Required]
+        [StringLength(8, MinimumLength = 4, ErrorMessage = "You must specify password btw 4 & 8 characters")]
+        public string Password { get; set; }
+    }
+}
