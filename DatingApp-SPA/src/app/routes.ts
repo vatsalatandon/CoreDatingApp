289d7378a8b3c61648fc4b72c39917ff6fc75024
vatsala.tandon <vatsala.tandon@CYG391>
2020-04-17 19:13:27 +0530

    Multiple routes to single route using dummy routes

--- DatingApp-SPA/src/app/routes.ts
+++ DatingApp-SPA/src/app/routes.ts
@@ -6,9 +6,16 @@ import { ListsComponent } from './lists/lists.component';
 import { AuthGuard } from './_guards/auth.guard';
 
 export const appRoutes: Routes = [
-    {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
-    {path: 'members', component: MemberListComponent, canActivate: [AuthGuard]},
-    {path: 'messages', component: MessagesComponent, canActivate: [AuthGuard]},
-    {path: 'lists', component: ListsComponent, canActivate: [AuthGuard]},
-    {path: '**', redirectTo: 'home', pathMatch: 'full'},
+    {path: '', component: HomeComponent},
+    {
+        path: '',
+        runGuardsAndResolvers: 'always',
+        canActivate: [AuthGuard],
+        children:[
+            {path: 'members', component: MemberListComponent},
+            {path: 'messages', component: MessagesComponent},
+            {path: 'lists', component: ListsComponent},
+        ]
+    },
+    {path: '**', redirectTo: '', pathMatch: 'full'},
 ];
